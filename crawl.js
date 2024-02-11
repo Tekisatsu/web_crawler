cce903f20445a7c3aabb2d63ec89e074c7dee79b
Tekisatsu <ale.hartikainen@gmail.com>
2024-02-11 17:33:41 +0200

    console input added

--- crawl.js
+++ crawl.js
@@ -1,12 +1,28 @@
 const url = require('node:url');
+const { JSDOM } = require('jsdom')
+
 
 function normalizeURL(oldUrl){
-      const newUrl = new URL(oldUrl);
-      let norm = `${newUrl.hostname}${newUrl.pathname}`;
-      norm = norm.replace(/\/$/, '');
-      return norm;
+      try {
+            const newUrl = new URL(oldUrl);
+            let norm = `${newUrl.hostname}${newUrl.pathname}`;
+            norm = norm.replace(/\/$/, '');
+            return norm;
+      } catch (error) {
+            console.log('Invalid URL')
+      }
+}
+function getURLfromHTML(html,rootUrl) {
+const { window } = new JSDOM(html);
+    const linkElements = window.document.querySelectorAll('a');
+    const urls = Array.from(linkElements).map(tag => {
+        return new URL(tag.getAttribute('href'), rootUrl).href;
+    });
+    return urls;
 }
 
+
 module.exports = {
-      normalizeURL
+      normalizeURL,
+      getURLfromHTML
 }
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+function main(){
+      const argv = process.argv.slice(2);
+      if (argv.length !== 1){
+            console.error('Invalid input. Provide one valid URL');
+            process.exit(1);
+      }
+      const baseURL = argv[0]
+      console.log(`crawler is starting ${baseURL}`)
+}
+
+main()
